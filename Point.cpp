64b46e574143408b6cc00f4a0516a97a8301227c
SHERAPVERSLUYS <>
2022-03-09 17:14:33 +0000

    WS1

--- Point.cpp
+++ Point.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
 #include "Point.h"
+#include <math.h>
+using namespace std;
+
 
 // Notice how when we use field names in the functions that
 // belong to Point, we can just use the name without a dot operator.
@@ -6,4 +10,32 @@ Point::Point() {
     x = 0.0;
     y = 0.0;
 }
+Point::Point(double xVal, double yVal){
+  x = xVal;
+  y = yVal;
+}
 
+//double Point::getX(){
+ // return x;
+//}
+//double Point::getY(){
+ // return y;
+//}
+
+void Point::setX(double xVal){
+  x = xVal;
+}
+void Point::setY(double yVal){
+  y = yVal;
+}
+
+void Point::print(){
+  cout << "x:" << x << " " << "y:" << y << endl;
+}
+
+double Point::calcDistance(Point other){
+double xVal = pow(x - other.x, 2);
+double yVal = pow(y - other.y, 2);
+double dist = sqrt(xVal + yVal);
+return dist;
+  }
